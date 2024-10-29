907b370bdbb0e3ea7a7a83f60b4d3160335da311
None None <kennbatacandolo221@gmail.com>
2024-10-29 12:01:44 +0100

    changed localhost

--- db.php
+++ db.php
@@ -1,28 +1,9 @@
 <?php
 
-// $servername = "localhost";
-// $username = "hr3_mfinance";
-// $password = "hr3_mfinance";
-// $dbname   = "hr3_mfinance";
-
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname, );
-
-// // Check connection
-// if (mysqli_connect_error()) {
-//     die("Database connection failed: " . mysqli_connect_error());
-//   }
-// else{
-    
-// }
-
-// return $conn;
-
-// for testing in localhost
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname   = "db_login";
+$username = "hr3_mfinance";
+$password = "hr3_mfinance";
+$dbname   = "hr3_mfinance";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname, );
@@ -35,5 +16,5 @@ else{
     
 }
 
-
 return $conn;
+
