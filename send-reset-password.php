84ff2c4f33df61d8b27a9f9dd65129fa536fb945
None None <kennbatacandolo221@gmail.com>
2025-03-09 08:50:39 +0100

    update the link message on email for reses pass

--- send-reset-password.php
+++ send-reset-password.php
@@ -3,44 +3,46 @@
 $email = $_POST['email'];
 
 $token = bin2hex(random_bytes(16));
+$token_hash = hash("sha256", $token);
 
-$token_hash = hash("sha256",$token);
-
-$expiry = date("Y-m-d H:i:s", time() + 60 * 10); 
+// Set token expiration to 10 minutes from now
+$expiry = date("Y-m-d H:i:s", time() + 60 * 10);
 
 $conn = require __DIR__ . "/db.php";
 
 $sql = "UPDATE users
         SET reset_token_hash = ?,
             reset_token_expires_at = ?
-        WHERE email =?";
+        WHERE email = ?";
 
 $stmt = $conn->prepare($sql);
-
 $stmt->bind_param("sss", $token_hash, $expiry, $email);
-
 $stmt->execute();
 
-
 if ($conn->affected_rows) {
-  $mail = require __DIR__ . "/mail.php";
-  $mail->setFrom("mfinance@email.com");
-  $mail->addAddress($email);
-  $mail->Subject = "Password Reset";
-  $mail->Body = <<<END
+    $mail = require __DIR__ . "/mail.php";
+    $mail->setFrom("mfinance@email.com");
+    $mail->addAddress($email);
+    $mail->Subject = "Password Reset";
 
-  Click <a href = "/home/hr3.microfinance-solution.com/public_html/reset-password.php?token=$token"> here </a>to reset your password
+    // Dynamically set the domain
+    $domain = $_SERVER['HTTP_HOST'];
+    
+    // Enable HTML in the email body
+    $mail->isHTML(true);
+    $mail->Body = <<<END
+    Click <a href="http://hr3.microfinance-solution.com/reset-password.php?token=$token">here</a> to reset your password.
+    END;
 
-  END;
-
-  try{
-    $mail->send();
-  } catch(Exception $e){
-      echo "Message could not be sent Mailer error: {$email->Errorinfo}";
-  }
+    try {
+        $mail->send();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 
-echo  "<script>
+echo "<script>
 alert('Message sent to your Email.');
 window.location.href = 'login.php'; // Redirect to login page
 </script>";
+?>
